c24e4174de024f2789b5cf8c1f58f63c77e58167
ehabel-kady <ehab@upwardli.com>
2023-05-25 13:35:16 +0300

    added allowed host IP

--- task_system/settings.py
+++ task_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hub#to#cu*keq2u)uj(@zncn%z!#owc(+=hzrfxkzie2rhmnay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.92.20.79']
 
 
 # Application definition
